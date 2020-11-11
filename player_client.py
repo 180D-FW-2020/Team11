71ee3df3f5edebc7b21134b3b4607f570a90f422
aliashraf99 <45701408+aliashraf99@users.noreply.github.com>
2020-11-11 23:30:53 +0200

    "Dummy" MQTT
    Given topic upon initialization
    Only sends and receives given messages - doesn't parse

--- player_client.py
+++ player_client.py
@@ -4,7 +4,6 @@ from paho.mqtt import client as mqtt_client
 import json
 broker = 'broker.emqx.io'
 port = 1883
-topic = "ece180d/team11"
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 
 class Transmitter:
@@ -15,37 +14,41 @@ class Transmitter:
             else:
                 print("Failed to connect, return code %d\n", rc)
 
-    def __on_message(self, client, userdata, message):
+  #  def __on_message(self, client, userdata, message):
         #filter data to get only json
         #parse message
-        self.message["messages"].append(json.loads(str(message.payload)[2:-1]))
+   #     self.message.append(json.loads(str(message.payload)[2:-1]))
 
-    def __init__(self):
+    def __init__(self, topic):
         self.client = mqtt_client.Client()
         self.topic = topic
         self.message = {"messages" :[]}
 
         self.client.on_connect = self.on_connect
         self.client.connect(broker, port)
-        self.client.on_message = self.__on_message
+    #    self.client.on_message = self.__on_message
 
         self.client.connect_async('mqtt.eclipse.org')
 
 
-    def update(self, PlayerID, Direction, Rotation):
-        msg = {
-            "Player ID" : PlayerID,
-            "Direction" : Direction,
-            "Rotation" : Rotation, 
-        }
+    def update(self, newinfo):
+   #     msg = {
+    #        "Player ID" : PlayerID,
+    #        "Direction" : Direction,
+    #        "Rotation" : Rotation, 
+    #    }
+        msg = newinfo
         self.message["messages"].append(msg)
+     #   self.message.append(PlayerID)
+      #  self.message.append(Direction)
+      #  self.message.append(Rotation)
         
 
     def send(self):
         self.client.loop_start()
-        self.client.publish(topic, json.dumps(self.message), qos=1)
+        self.client.publish(self.topic, json.dumps(self.message), qos=1)
         print(json.dumps(self.message))
-        self.message = {"messages":[]}
+        self.message = {"messages" :[]}
         self.client.loop_stop()
       #  self.client.disconnect()
 
@@ -53,14 +56,14 @@ class Transmitter:
 
 
 if __name__ == '__main__':
-    player1 = Transmitter()
+    player1 = Transmitter("ece180d/team11")
    # player1.update(1, 1, 1)
   #  player1.send()
-    player1.update(1,0,2)
+    player1.update("it's a good day")
     player1.send()
-    player1.update(0,0,2)
+    player1.update(2)
     player1.send()
-    player1.update(4,0,2)
-    player1.send()
-    player1.update(7,3,2)
-    player1.send()
+    #player1.update(4,0,2)
+    #player1.send()
+    #player1.update(7,3,2)
+    #player1.send()
--- player_sub.py
+++ player_sub.py
@@ -1,5 +1,5 @@
 import random
-
+import re
 from paho.mqtt import client as mqtt_client
 
 
@@ -22,7 +22,7 @@ class Reciever:
 
     def on_connect(self, client, userdata, flags, rc):
         if rc == 0:
-            self.client.subscribe(topic)
+            self.client.subscribe(self.topic)
             print("Connected to Player Communication Channel!")
         else:
             print("Failed to connect, return code %d\n", rc)
@@ -30,21 +30,33 @@ class Reciever:
     def __on_message(self, client, userdata, msg):
         
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}`")
+        message = msg.payload.decode()
+       # info = re.findall(r'\d+', message)
+       # playerID = int(info[0])
+       # Direction = int(info[1])
+       # Rotation = int(info[2])
+       # print("player ID : ", {playerID}, ", Direction: ",{Direction}, "Rotation :", {Rotation})
+        self.received = True
 
-    def __init__(self):
+    def __init__(self, topic):
         self.client = mqtt_client.Client(client_id)
         self.client.on_connect = self.on_connect
         self.client.on_message = self.__on_message
         self.client.connect(broker, port)
         self.topic = topic
-        print(self.topic)
+        self.received = False
 
         self.client.connect_async('mqtt.eclipse.org')
 
-    def recieve(self, duration= 0):
+    def recieve(self, duration):
             if duration == 0:
                 self.client.loop_start()
                 while True:
+                    pass
+                self.client.loop_stop()
+            if duration == -1:
+                self.client.loop_start()
+                while self.received == False:
                     pass
                 self.client.loop_stop()
             else:
@@ -56,7 +68,7 @@ class Reciever:
 
 
 if __name__ == '__main__':
-    player1 = Reciever()
-    player1.recieve(20)
+    player1 = Reciever("ece180d/team11")
+    player1.recieve(0)
 
 
