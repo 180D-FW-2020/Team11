f4ba343dde2a7e8ac185172ca48e1a89228a4c17
combobulate <zefyr.scott@gmail.com>
2020-11-11 17:00:48 -0800

    fixing bad file issue

--- gameplay.py
+++ gameplay.py
@@ -1,25 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  6 20:46:40 2020
+
+@author: zefyr
+"""
+import random as r
+
 ROTATION_COOLDOWN = 20
 
 class GamePlay:
-    def __init__(self, primaryNode = True):
     def __init__(self, numPlayers, primaryNode = True):
         try:
             if primaryNode:
                 args = self.settings()
-            else: args = (0, 0, 0, 0)
-            self.startPlaySpace(args)
             else: args = (numPlayers, 0, 0, 0)
-
+            
             self.playSpace = PlaySpace(numPlayers, args)
         except:
             print("An error occurred initializing GamePlay")
-
+    
     def settings(self):
-        try:
-            print("This will prompt a player for the game settings")
-            # use mic to get edgeLength, numObstacles, numPowerups, this is dummy
-            numPlayers, edgeLength, numObstacles, numPowerups = (0, 0, 0, 0)
-            return numPlayers, edgeLength, numObstacles, numPowerups
         '''
         This prompts the central node player for game settings: edgelength,
         number of obstacles, and number of powerups. Number of players too
@@ -33,35 +33,21 @@ class GamePlay:
             return edgeLength, numObstacles, numPowerups
         except:
             print("An error occurred getting settings")
-
-    def startPlaySpace(self, args):
-        try:
-            self.playSpace = PlaySpace(args)
-        except:
-            print("An error occurred generating the playspace")
+    
     def unpack(self):
         '''
         Unpacks message from a pc or pi containing a player number with a
         rotation or direction.
             
-    def getPlayerInput(self):
         Returns playerId, rotation, and direction.
         '''
         try:
-            print("This will get package date from players and", 
-                  "return a player number with a rotation",
-                  "or direction")
-            #return rotation, direction
             # These are dummy values
             playerId, rotation, direction = (0, 0, 0)
             return playerId, rotation, direction
         except:
             print("An error occurred getting player input")
-
-    def updateAllNodes(self):
-        try:
-            print("This sends a message to all nodes with the current playspace",
-                  "and player data, and clears nodes to send new messages")
+    
     def pack(self):
         '''
         Packs message for pc and pi containing updates for the display, permission
@@ -76,33 +62,27 @@ class GamePlay:
             return message
         except:
             print("An error occurred updating nodes")
-
+            
 class PlaySpace:
     def __init__(self, numPlayers, edgeLength, numObstacles, numPowerups):
         try:
             self.edgeLength = edgeLength
             self.obstacles = []
             self.powerUps = []
-<<<<<<< Updated upstream:gameplay.py
-            self.message = ""
-            self.sender = Transmitter("team11/nodetoPC")
-            #sender.update("all player xyz and it, plane of referance")
-=======
-            self.sender = Trasmitter("team11/nodetopi")
->>>>>>> Stashed changes:gamePlay.py
             if numObstacles: self.placeObstacles(numObstacles)
             if numPowerups: self.placePowerUps(numPowerups)
-
-@@ -62,92 +78,128 @@ def __init__(self, numPlayers, edgeLength, numObstacles, numPowerups):
-
+            
+            self.verticalAxis = 1
+            self.horizontalAxis = 2
+            self.rotationCoolDownRemaining = 0
+            
             if numPlayers:
                 self.numPlayers = numPlayers
-                self.playersNotIt = self.placePlayers(numPlayers)
                 self.players, self.playersNotIt = self.placePlayers(numPlayers)
 
         except:
             print("An error occurred initializing PlaySpace")
-
+            
     def placePlayers(self, numPlayers):
         '''
         Randomly places players in the playspace with respect to the edgelength
@@ -110,114 +90,74 @@ class PlaySpace:
         of players, and a list of just the players who are not It.
         '''
         try:
-            it = r.randrange(0, numPlayers, 1)
             playerIt = r.randrange(1, numPlayers+1, 1)
             players = []
-            for i in range (numPlayers):
             playersNotIt = []
             for i in range (1, numPlayers+1):
                 # Get a random position a reasonable distance from other players
                 # with respect to edge length. Don't use origin, this is dummy code
                 x, y, z = (0, 0, 0)                
-                if(i == it): it = True
-                else: it = False
                 if(i == playerIt): it = True
                 else:
                     it = False
                     playersNotIt.append(i)
                 players.append(Player(i, x, y, z, it))
-<<<<<<< Updated upstream:gameplay.py
-
-=======
-                self.sender.update("new player postion, it")
-                self.sender.send()
             
->>>>>>> Stashed changes:gamePlay.py
             return players, playersNotIt
         except:
             print("An error occurred placing players")
-
+            
     def placeObstacles(self, numObstacles):
         '''
         Randomly places obstacles in the playspace with respect to the edgelength
         and any other obstacles.
         '''
         try:
-            print("This will place up to number of obstacles in appropriately",
-                    "randomized positions, with respect to edge length")
             # Add obstacle code here
             pass
         except:
             print("An error occurred placing obstacles")
-
+    
     def placePowerUps(self, numPowerUps):
         '''
         Randomly places powerups in the playspace with respect to the edgelength
         and any other powerups.
         '''
         try:
-            print("This will place up to number of powerups in appropriately",
-                    "randomized positions, with respect to edge length")
             # Add powerUp code here
             pass
         except:
             print("An error occurred placing powerups")
-
-    def movePlayer(self, player, direction):
+            
     def movePlayer(self, playerId, direction):
         '''
         Takes a player ID and direction and checks if a move is possible.
         '''
         try:
             # First check for collision
-            collision, tag, powerup = self.checkCollision(player, direction)
             collision, tag, powerup = self.checkCollision(playerId, direction)
             # If collision is a tag, do the tagging stuff
             # If collision is obstacle/wall/non tag player bump, do nothing
             # Otherwise move. If there's a powerup there, pick it up
-<<<<<<< Updated upstream:gameplay.py
-            #sender.update("playerID , updated locations")
-            #send.send()
-=======
-            self.sender.update("new player postion")
-            self.sender.send()
->>>>>>> Stashed changes:gamePlay.py
             if(tag):
-                tag.setIt()
-                player.setNotIt()
-                if player in self.playersNotIt:
-                    self.playersNotIt.remove(player)
-                     #sender.update("playerID , updated it var")
-                    #send.send()
                 self.players[tag - 1].setIt()
                 self.players[playerId - 1].setNotIt()
                 if tag.PlayerId in self.playersNotIt:
                     self.playersNotIt.remove(tag)
-
+                
         except:
-            print("An error occurred moving player", player, ":", direction)
             print("An error occurred moving player", playerId, ":", direction)
 
-    def rotatePlaySpace(self, player, rotation):
     def rotatePlaySpace(self, rotation):
         '''
         Takes a rotation, rotates the playspace.
         '''
         try:
-            print("This will attempt to rotate the playspace")
             # write this
-<<<<<<< Updated upstream:gameplay.py
-             #sender.update("playerID , updated rotation")
-            #send.send()
-=======
-            self.sender.update("new referance plane")
-            self.sender.send()
->>>>>>> Stashed changes:gamePlay.py
             pass
         except:
-            print("An error occurred rotating", rotation, "from player", player)
             print("An error occurred rotating", rotation)
-
+    
     def checkCollision(self, player, direction):
         '''
         Takes a player and direction, figures out if they are going to run into 
@@ -229,22 +169,15 @@ class PlaySpace:
          - tag: if the moving player is it and the obstacle is another
              player, this is the other playerId, else 0
          - powerup: if the obstacle is a powerup, this is the index for it
+
         '''
         try:
-            print("This will check if an obstacle exists where the player wants",
-                  "to move")
-            # Return tuple (collision, tag, powerup):
-            #  - collision: bool indicating a collision with obstacle, edge, or
-            #       another player where the moving player is not it
-            #  - tag: if the moving player is it and the obstacle is another
-            #       player, this is the other player, else 0
-            #  - powerup: if the obstacle is a powerup, this is the index for it
             # These are dummy vals
             collision, tag, powerup = (0,0,0)
             return collision, tag, powerup
         except:
             print("An error occurred checking collision")
-
+    
     def setRotationCoolDown(self):
         '''
         Starts the rotation cooldown
@@ -253,7 +186,7 @@ class PlaySpace:
             self.rotationCoolDownRemaining = ROTATION_COOLDOWN
         except:
             print("An error occurred setting the rotation cooldown")
-
+    
     def rotationCoolDown(self):
         '''
         Counts down the rotation cooldown.
@@ -261,21 +194,19 @@ class PlaySpace:
         try:
             if(self.rotationCoolDownRemaining>0): self.rotationCoolDownRemaining -= 1
         except:
-            print("An error occurred decrementing the rotation cooldown")
             print("An error occurred decrementing the rotation cooldown")        
-
+            
 class Player:
-    def __init__(self, playerNum, x, y, z, it):
     def __init__(self, playerId, x, y, z, it):
         try:
-            self.playerNum = playerNum;
             self.playerId = playerId;
             self.x = x
             self.y = y
             self.z = z
-@@ -156,6 +208,9 @@ def __init__(self, playerNum, x, y, z, it):
+            self.it = it
+        except:
             print("An error occurred initializing Player")
-
+    
     def setPosition(self, x, y, z):
         '''
         Sets the player position values
@@ -283,7 +214,8 @@ class Player:
         try:
             self.x = x
             self.y = y
-@@ -164,15 +219,22 @@ def setPosition(self, x, y, z):
+            self.z = z
+        except:
             print("An error occurred updating player position")
 
     def setIt(self):
@@ -291,7 +223,6 @@ class Player:
         Sets the player as It
         '''
         try:
-            if(not self.it): self.it = True
             if(not self.it):
                 self.it = True
             else: print("Tagged person was already it, something went wrong")
@@ -306,5 +237,4 @@ class Player:
             if(self.it): self.it = False
             else: print("Tagged person was already not it, something went wrong")
         except:
-            print("An error occurred updating who is it")             
-            print("An error occurred updating who is it") 
+            print("An error occurred updating who is it")
--- player_client.py
+++ player_client.py
@@ -4,6 +4,7 @@ from paho.mqtt import client as mqtt_client
 import json
 broker = 'broker.emqx.io'
 port = 1883
+topic = "ece180d/team11"
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 
 class Transmitter:
@@ -19,7 +20,7 @@ class Transmitter:
         #parse message
    #     self.message.append(json.loads(str(message.payload)[2:-1]))
 
-    def __init__(self, topic):
+    def __init__(self):
         self.client = mqtt_client.Client()
         self.topic = topic
         self.message = []
@@ -31,23 +32,23 @@ class Transmitter:
         self.client.connect_async('mqtt.eclipse.org')
 
 
-    def update(self, newinfo):
-   #     msg = {
+    def update(self, PlayerID, Direction, Rotation):
+    #    msg = {
     #        "Player ID" : PlayerID,
-    #        "Direction" : Direction,
-    #        "Rotation" : Rotation, 
-    #    }
-        msg = newinfo
-        self.message["messages"].append(msg)
-     #   self.message.append(PlayerID)
-      #  self.message.append(Direction)
-      #  self.message.append(Rotation)
-      
+     #       "Direction" : Direction,
+     #       "Rotation" : Rotation, 
+     #   }
+      #  self.message["messages"].append(msg)
+        self.message.append(PlayerID)
+        self.message.append(Direction)
+        self.message.append(Rotation)
+        
+
     def send(self):
         self.client.loop_start()
-        self.client.publish(self.topic, json.dumps(self.message), qos=1)
+        self.client.publish(topic, json.dumps(self.message), qos=1)
         print(json.dumps(self.message))
-        self.message = {"messages" :[]}
+        self.message = []
         self.client.loop_stop()
       #  self.client.disconnect()
 
@@ -55,12 +56,12 @@ class Transmitter:
 
 
 if __name__ == '__main__':
-    player1 = Transmitter("ece180d/team11")
+    player1 = Transmitter()
    # player1.update(1, 1, 1)
   #  player1.send()
-    player1.update("it's a good day")
+    player1.update(1,0,2)
     player1.send()
-    player1.update(2)
+    player1.update(0,0,2)
     player1.send()
     #player1.update(4,0,2)
     #player1.send()
