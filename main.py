1f8fe73facc5415aa6f807a604be61101fb22688
aliashraf99 <ali.ashraf1410@outlook.com>
2020-11-20 02:25:46 +0200

    Threading integration

--- main.py
+++ main.py
@@ -5,22 +5,19 @@ Created on Mon Nov  9 12:54:08 2020
 @author: zefyr
 """
 
-# Should have better way to set these values than hard coding
-isPi = False
-isPrimary = True
-playerId = 1
-numPlayers = 4
-
 import gamePlay as g
-if isPi: import playerPi
-else: import playerPC
+import playerPi
+import playerPC
 import player_sub as sub
 import player_client as cli
 from threading import Thread
-import cv2
+import multiprocessing
 
-nodeToPrimary = "ece180d/team11/nodeToPrimary"
-primaryToNode = "ece180d/team11/primaryToNode"
+# Should have better way to set these values than hard coding
+isPi = False
+isPrimary = False
+playerId = 1
+numPlayers = 4
             
 def piProcess():
     '''
@@ -32,22 +29,27 @@ def piProcess():
         * add threading to support simultaneous listen for display update and
             player action
     '''
-    subscriber = sub.Receiver(primaryToNode)
-    client = cli.Transmitter(nodeToPrimary)
+    subscriber = sub.Receiver("primaryNode")
+    client = cli.Transmitter("player")
     
     pi = playerPi.PlayerPi(playerId)   
     canSend = True
     
     while True:
+        receiverProcess = Thread(target= subscriber.receive(), args= (-1,))
+        receiverProcess.start()
         if canSend:
             rotation = pi.getRotation()
             package = pi.pack(rotation)
             client.update(package)
-            client.send()
+            senderProcess = Thread(target= client.send())
+            senderProcess.start()
             canSend = False
-        else:
-            message = subscriber.receive(-1)
-            canSend = pi.unpack(message)
+        receiverProcess.join()
+        canSend = pi.unpack(subscriber.message)
+        if (senderProcess.is_alive):
+            senderProcess.join()
+        
 
 def pcProcess():
     '''
@@ -58,31 +60,33 @@ def pcProcess():
     received message.
     
     Currently requires changes:
-        * add threading to support simultaneous listen for direction and rotation
+        * add threading to support simulataneous listen for direction and rotation
         * add threading to support simultaneous listen for display update and
             player action
     '''
-    print("PC connecting")
-    subscriber = sub.Receiver(primaryToNode)
-    client = cli.Transmitter(nodeToPrimary)
+    subscriber = sub.Receiver("primaryNode")
+    client = cli.Transmitter("player")
     
     pc = playerPC.PlayerPC(playerId, numPlayers)
     canSend = True
     
-    display = Thread(target=displayProcess, args=(pc, ))
+    display = Thread(target=displayProcess, args=(pc))
     display.start()
     
     while True:
+        receiverProcess = Thread(target= subscriber.receive(), args= (-1,))
+        receiverProcess.start()
         if canSend:
-            print("pc checking for direction")
             direction = pc.getDirection()
             package = pc.pack(direction)
             client.update(package)
-            client.send()
+            senderProcess = Thread(target= client.send())
+            senderProcess.start()
             canSend = False
-        else:
-            message = subscriber.receive(-1)
-            canSend = pc.unpack(message)
+        receiverProcess.join()
+        canSend = pi.unpack(subscriber.message)
+        if (senderProcess.is_alive):
+            senderProcess.join()
             
 def centralNodeProcess():
     '''
@@ -92,63 +96,54 @@ def centralNodeProcess():
     When a message is received, the PlaySpace and game state are updated, and
     a message is sent to player PCs and Pi's indicating the update.
     '''
-    print("central connecting")
-    subscriber = sub.Receiver(nodeToPrimary)
-    client = cli.Transmitter(primaryToNode)
+    subscriber = sub.Receiver("player")
+    client = cli.Transmitter("primaryNode")
     
     game = g.GamePlay(numPlayers, primaryNode = True)
-    # This is for the initial playspace transmission. Update with threading to
-    # transmit continuously while receiving messages from players until all
-    # players confirm receipt.
-    message = game.pack()
-    client.update(message)
-    client.send()
     
     while True:
-        print("central looking for message")
-        message = subscriber.receive(-1)
+        receiverProcess = Thread(target= subscriber.receive(-1))
+        receiverProcess.join()
+        message = subscriber.message
         playerId, direction, rotation = game.unpack(message)
         if (direction):
-            message = game.playSpace.movePlayer(playerId, direction)
+            game.playSpace.movePlayer(playerId, direction)
         elif (rotation):
-            message = game.playSpace.rotatePlaySpace(rotation)
+            game.playSpace.rotatePlaySpace(rotation)
         else:
             # This shouldn't happen
             print("A message was received without direction or rotation")
-        
-        if message:
-            client.update(message)
-            client.send()
+            
+        message = game.pack()
+        client.update(message)
+        senderProcess = Thread(target= client.send)
+        senderProcess.join()
 
 def displayProcess(pc):
     '''
     Updates the display as needed.
     '''
+    
     while True:
-        if pc.displayUpdate:
-            pc.updateDisplay()
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cv2.destroyAllWindows()
+        if pc.displayUpdate: pc.updateDisplay()
 
 ### Select processes to run for instance
-if __name__ == '__main__':
-    if isPi:
-        try:
-            piProcess()
-        except:
-            print("An error occurred with pi processes")
-    elif isPrimary:
-        try:
-            central = Thread(target=centralNodeProcess)
-            player = Thread(target=pcProcess)
-            central.start()
-            player.start()
-        except:
-            print("An error occurred with primary node processes")
-    else:
-        # Only other case is this is the playerPC
-        try:
-            pcProcess()
-        except:
-            print("An error occurred with non primary node processes")
+if isPi:
+    try:
+        piProcess()
+    except:
+        print("An error occurred with pi processes")
+elif isPrimary:
+    try:
+        central = multiprocessing.Process(target=centralNodeProcess)
+        player = multiprocessing.Process(target=pcProcess)
+        central.start()
+        player.start()
+    except:
+        print("An error occurred with primary node processes")
+else:
+    # Only other case is this is the playerPC
+    try:
+        pcProcess()
+    except:
+        print("An error occurred with non primary node processes")
