86779a50b4114e147277ec363ada35e024ecfd9c
aliashraf99 <ali.ashraf1410@outlook.com>
2021-03-06 01:56:01 +0200

    display fix

--- playerPC.py
+++ playerPC.py
@@ -579,6 +579,7 @@ class PlayerPC:
                 print("player", message['playerId'], "registered as", self.playerId, "has activated speed")
             if self.playerId == message['playerId']:
                 cv2.rectangle(display, (540,920), (1500, 1000), (0,0,0), -1)
+                cv2.putText(display, "No powerups held!", (550,960), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
                 cv2.putText(display, "RUN! - Speed powerup is active", (550,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
             if settings.verbose:
                 print("powerup held is", self.playSpace.players[message['playerId']-1]['powerUpHeld'])
@@ -595,6 +596,7 @@ class PlayerPC:
                 print("player", message['playerId'], "registered as", self.playerId, "has activated freeze")
             if self.playerId == message['playerId']:
                 cv2.rectangle(display, (540,920), (1500, 1000), (0,0,0), -1)
+                cv2.putText(display, "No powerups held!", (550,960), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
                 cv2.putText(display, "RUN! - Everyone else is frozen", (550,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
             else:
                 cv2.rectangle(display, (540,0), (1500, 40), (0,0,0), -1)
