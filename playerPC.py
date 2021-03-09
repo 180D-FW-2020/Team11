0b05a338d2b1c0bf2fa9025ac5b8059aded8598e
combobulate <zefyr.scott@gmail.com>
2021-03-08 16:05:42 -0800

    Fix powerup pickup display behavior

--- playerPC.py
+++ playerPC.py
@@ -713,6 +713,7 @@ class PlayerPC:
         oldPowerUp = self.playSpace.powerUps.pop(message['index'])
         self.powerUp = oldPowerUp['powerUp']
         oldpos = oldPowerUp['position']
+        newpos = message['positionpower']
         newPower = {'powerUp': message['powerUp'],
                     'position': message['positionpower']}
         self.playSpace.powerUps.append(newPower)
@@ -734,10 +735,10 @@ class PlayerPC:
         self.pickupSound.play()
         
         # Place in new position
-        hpos = np.dot(self.playSpace.horizontalAxis, self.playSpace.powerUps[-1]['position'])
+        hpos = np.dot(self.playSpace.horizontalAxis, newpos)
         if hpos<0:
             hpos = self.playSpace.edgeLength + hpos + 1
-        vpos = -1*np.dot(self.playSpace.verticalAxis, self.playSpace.powerUps[-1]['position'])
+        vpos = -1*np.dot(self.playSpace.verticalAxis, newpos)
         if vpos<0:
             vpos = self.playSpace.edgeLength + vpos + 1
         # display = cv2.circle(display,(self.dist*hpos + int(self.dist/2), self.dist*vpos + int(self.dist/2)),
